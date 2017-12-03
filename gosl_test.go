0ae13029254810586bf9fc405f5c5702e2fc67fb
Gwyneth Llewelyn <gwyneth.llewelyn@gwynethllewelyn.net>
2017-12-03 18:11:19 +0000

    Memory tests. go test -v will show mmap being used on files of increased RAM size, in multiples of 128 Mbytes.

--- gosl_test.go
+++ gosl_test.go
@@ -4,18 +4,25 @@ import "os"
 import "testing"
 import "github.com/dgraph-io/badger/y"
 
-var Size int64 = 256 * 1024 * 1024
+var Size int64 = 128 * 1024 * 1024
 
 func TestMmap(t *testing.T) {
 	t.Log("Trying mmap")
-	flags := os.O_RDWR | os.O_CREATE | os.O_EXCL
-	f, err := os.OpenFile("test.md", flags, 0666)
-	defer f.Close()
-	y.Check(err)
-//	size := int64(256 * 1024 * 1024)
-	t.Logf("Size is : %v", Size)
-	_, err = y.Mmap(f,false,Size)
-	if err != nil {
-		t.Errorf("mmap failed with error: %v", err)
+	var i int64
+	for i = 1; i < 8; i++ {
+		flags := os.O_RDWR | os.O_CREATE | os.O_EXCL
+		f, err := os.OpenFile("test.md", flags, 0666)
+		defer f.Close()
+		y.Check(err)
+	//	size := int64(256 * 1024 * 1024)
+		t.Logf("Size is : %v", i*Size)
+		_, err = y.Mmap(f,false,i*Size)
+		if err != nil {
+			t.Errorf("mmap failed with error: %v", err)
+		}
+		err = os.Remove("test.md")
+		if err != nil {
+			t.Errorf("could not remove test.md: %v", err)
+		}
 	}
 }
